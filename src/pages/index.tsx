f77c47a586bf9ba7dcd3562601195564b363decd
Anurag <anuragsharma011011@gmail.com>
2022-09-09 10:10:53 +0530

    route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from "next/router";
+
 
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
   return (
-   <>Hello World</>
+   <>
+    <Head>
+      <title>Ideas-AI</title>
+    </Head>
+   <div>
+    Hello World
+    </div> 
+    <button onClick={() => router.push("/idea")}  >Try Now </button>
+   </>
   )
 }
 
